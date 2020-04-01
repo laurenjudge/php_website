2f1afc3cd160c66317a5965ab637ca71fa55bd1c
Lauren Judge <48807725+laurenjudge@users.noreply.github.com>
2020-04-02 10:31:46 +1100

    Templates added

---
+++ templates/header.php
@@ -0,0 +1,17 @@
+<head>
+  <title></title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+</head>
+<body class="grey lighten-4"> 
+  <nav class="white z-depth-0">
+    <div class="container">
+      <a href="/" class="brand-logo black-text">
+        Welcome
+      </a>
+      <ul class="right hide-on-small-and-down" id="nav-mobile">
+        <li>
+          <a href="#" class="brand btn z-depth-0">Add an Item</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
